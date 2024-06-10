8e59b091aa426b029352c4192ca80b3d2d8c8f57
itz-shakil-92 <shakilkathat5603@gmail.com>
2024-06-11 00:11:20 +0530

    Created user model

--- vege/views.py
+++ vege/views.py
@@ -7,6 +7,12 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Q,Sum
+
+from django.contrib.auth import get_user_model
+
+User=get_user_model()
+
+#create your views
 #function for adding the receipe
 @login_required(login_url="/login/")
 def receipes(request):
